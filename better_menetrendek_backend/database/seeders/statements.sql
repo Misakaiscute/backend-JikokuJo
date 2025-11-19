f96ddb430bc211dd527ab9c1837ec355ffa775e0
3111TAN-05 <3111TAN-05@tanuzem.boronkay.local>
2025-11-19 12:21:59 +0100

    jo_sql

--- better_menetrendek_backend/database/seeders/statements.sql
+++ better_menetrendek_backend/database/seeders/statements.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `trips` (
   `trip_id` VARCHAR(255) NOT NULL,
-  `route_id` INT NOT NULL,
+  `route_id` varchar(255) NOT NULL,
   `service_id` VARCHAR(255) NOT NULL,
   `trip_headsign` VARCHAR(255) NOT NULL,
   `direction_id` INT NOT NULL,
@@ -8,10 +8,10 @@ CREATE TABLE `trips` (
   `shape_id` VARCHAR(255) NOT NULL,
   `wheelchair_accessible` TINYINT NOT NULL,
   `bikes_allowed` TINYINT NOT NULL,
-  PRIMARY KEY (`trip_id`)
-);
+  PRIMARY KEY (`trip_id`, `service_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `stops` (
-  `stop_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `stop_id` INT PRIMARY KEY,
   `stop_name` VARCHAR(255) NOT NULL,
   `stop_lat` DOUBLE NOT NULL,
   `stop_lon` DOUBLE NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `stops` (
   `location_sub_type` TINYINT NOT NULL,
   `parent_station` INT NOT NULL,
   `wheelchair_boarding` TINYINT NOT NULL
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `stop_times` (
   `trip_id` VARCHAR(255) NOT NULL,
   `stop_id` INT NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE `stop_times` (
   PRIMARY KEY (`trip_id`, `stop_id`, `stop_sequence`),
   FOREIGN KEY (`trip_id`) REFERENCES `trips`(`trip_id`),
   FOREIGN KEY (`stop_id`) REFERENCES `stops`(`stop_id`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `shapes` (
   `shape_id` VARCHAR(255) NOT NULL,
   `shape_pt_sequence` INT NOT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE `shapes` (
   `shape_pt_lon` DOUBLE NOT NULL,
   `shape_dist_traveled` DOUBLE NOT NULL,
   PRIMARY KEY (`shape_id`, `shape_pt_sequence`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `agency` (
   `agency_id` VARCHAR(255) NOT NULL,
   `agency_name` VARCHAR(255) NOT NULL,
@@ -51,9 +51,9 @@ CREATE TABLE `agency` (
   `agency_lang` VARCHAR(255) NOT NULL,
   `agency_phone` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`agency_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `routes` (
-  `route_id` INT NOT NULL AUTO_INCREMENT,
+  `route_id` varchar(255) NOT NULL,
   `agency_id` VARCHAR(255) NOT NULL,
   `route_short_name` VARCHAR(255),
   `route_long_name` VARCHAR(255),
@@ -63,8 +63,8 @@ CREATE TABLE `routes` (
   `route_text_color` VARCHAR(255) NOT NULL,
   `route_sort_order` SMALLINT NOT NULL,
   PRIMARY KEY (`route_id`),
-  CONSTRAINT `fk_routes_agency` FOREIGN KEY (`agency_id`) REFERENCES `agency`(`agency_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  FOREIGN KEY (`agency_id`) REFERENCES `agency`(`agency_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `pathways` (
   `pathway_id` INT NOT NULL,
   `pathway_mode` TINYINT NOT NULL,
@@ -73,9 +73,9 @@ CREATE TABLE `pathways` (
   `to_stop_id` INT NOT NULL,
   `traversal_time` INT NOT NULL,
   PRIMARY KEY (`pathway_id`),
-  CONSTRAINT `fk_pathways_from_stop` FOREIGN KEY (`from_stop_id`) REFERENCES `stops`(`stop_id`),
-  CONSTRAINT `fk_pathways_to_stop` FOREIGN KEY (`to_stop_id`) REFERENCES `stops`(`stop_id`)
-);
+  FOREIGN KEY (`from_stop_id`) REFERENCES `stops`(`stop_id`),
+  FOREIGN KEY (`to_stop_id`) REFERENCES `stops`(`stop_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `feed_info` (
   `feed_id` VARCHAR(255) NOT NULL,
   `feed_publisher_name` VARCHAR(255) NOT NULL,
@@ -85,19 +85,20 @@ CREATE TABLE `feed_info` (
   `feed_end_date` INT NOT NULL,
   `feed_version` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`feed_id`)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `calendar_dates` (
   `service_id` VARCHAR(255) NOT NULL,
   `date` INT NOT NULL,
-  `exception_type` TINYINT NOT NULL,
-  PRIMARY KEY (`service_id`, `date`)
-);
+  `exception_type` TINYINT NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE TABLE `users` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `id` INT AUTO_INCREMENT,
   `first_name` VARCHAR(255) DEFAULT NULL,
   `second_name` VARCHAR(255) DEFAULT NULL,
   `email` VARCHAR(255) DEFAULT NULL,
   `email_verified_at` TIMESTAMP NULL DEFAULT NULL,
   `password` VARCHAR(255) DEFAULT NULL,
-  `is_guest` TINYINT(1) NOT NULL DEFAULT 0
-);
+  `is_guest` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci
+
--- better_menetrendek_backend/routes/console.php
+++ better_menetrendek_backend/routes/console.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use database\Seeders\DatabaseSeeder;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 
-Schedule::call(DatabaseSeeder::populate_database())
-    ->twiceDaily(0, 12)
-    ->when(function () {
-        $datesInDatabase = DB::table('feed_info')->pluck('feed_end_date');
-        foreach ($datesInDatabase as $dateStr) {
-            $date = Carbon::parse($dateStr);
-            if ($date->isToday() || $date->isTomorrow()) {
-                return true;
-            }
-        }
-        return false;
-    });
+// Schedule::call(DatabaseSeeder::populate_database())
+//     ->twiceDaily(0, 12)
+//     ->when(function () {
+//         $datesInDatabase = DB::table('feed_info')->pluck('feed_end_date');
+//         foreach ($datesInDatabase as $dateStr) {
+//             $date = Carbon::parse($dateStr);
+//             if ($date->isToday() || $date->isTomorrow()) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     });
