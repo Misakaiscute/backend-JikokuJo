b135708596545eee41edfbffde7e64dfc8c5fbb6
K054D <k054.daniel@gmail.com>
2025-12-15 12:41:39 +0100

    kész a menetrend backend

--- better_menetrendek_backend/app/Http/Controllers/StopController.php
+++ better_menetrendek_backend/app/Http/Controllers/StopController.php
@@ -4,25 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Trip;
 
 class StopController extends Controller
 {
-    public function index_stops()
+    public function getRoutesForStopId(string $stopId)
     {
-        // try
-        // {
-        //     $stops = DB::table("stops")->distinct()->pluck("name");
-        //     return response()->json([
-        //         'stops' => $stops
-        //     ],
-        //     []);
-        // }
-        // catch(\Exception $e)
-        // {
-        //     return response()->json([
-        //         'stops' => $stops
-        //     ],
-        //     ['error' => $e->getMessage()]);
-        // }
+        \Log::info('Requested stop_id: ' . $stopId);
+        $count = DB::table('stop_times')->where('stop_id', $stopId)->count();
+        \Log::info('Stop times count for this stop: ' . $count);
+
+
+
+        $results = DB::table('stop_times as st')
+            ->join('trips as t', 'st.trip_id', '=', 't.id')
+            ->join('routes as r', 't.route_id', '=', 'r.id')
+            ->where('st.stop_id', $stopId)
+            ->select([
+                'r.id',
+                'r.short_name',
+                'r.long_name',
+                'r.type',
+                'st.arrival_time',
+                'st.departure_time',
+                'st.stop_sequence',
+                't.shape_id',
+                't.id',
+                't.trip_headsign'
+            ])
+            ->orderBy('r.short_name')
+            ->orderBy('st.departure_time')
+            ->get()
+            ->groupBy('route_id')
+            ->map(function ($group) {
+                $first = $group->first();
+    
+                return [
+                    'route_id'          => $first->id,
+                    'short_name'  => $first->short_name,
+                    'type'        => $first->type,
+                    'trips' => $group->map(function ($item) {
+                        return [
+                            'arrival_time'    => $item->arrival_time,
+                            'shape_id'        => $item->shape_id,
+                            'trip_headsign'   => $item->trip_headsign,
+                        ];
+                    })->values()->all()
+                ];
+            })->values()->all();
+    
+        return response()->json([
+            'data' => $results,
+            'errors' => []
+        ]);
     }
 }
--- better_menetrendek_backend/routes/api.php
+++ better_menetrendek_backend/routes/api.php
@@ -2,15 +2,19 @@
 
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\RouteController;
+use App\Http\Controllers\StopController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/queryables', [SearchController::class, 'queryables']);  
+Route::get('/queryables', [SearchController::class, 'queryables']);
 //összes megállónév + id-jük tömbösítve, + járatnevek
 
-Route::get('/routes/{routeId}/fromWhere/{stopId}/date/{date}', [RouteController::class, 'getArrivalTimesByRouteId']);  
+Route::get('/routes/{routeId}/fromWhere/{stopId}/date/{date}', [RouteController::class, 'getArrivalTimesByRouteId']);
 //egy route_id-ből és stop_id-ből visszaadja az indulási időt + shapeId-t az adott dátumom
 
-Route::get('/routes/{routeId}/details/{shapeId}', [RouteController::class, 'getRouteStops']); 
+Route::get('/routes/{routeId}/details/{shapeId}', [RouteController::class, 'getRouteStops']);
 //a routeId és shaeId-val egy konkrét utat ad vissza a shape pontokkal együtt
+
+Route::get('/routes/{stopId}', [StopController::class, 'getRoutesForStopId']);
+//visszaadja az összes járatot + shapeId-t ami érinti a megállót
