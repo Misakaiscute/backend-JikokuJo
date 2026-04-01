cc1d1ca211dcf8dac894a98579f3e3354375fc10
3111TAN-05 <3111TAN-05@tanuzem.boronkay.local>
2026-04-01 10:25:04 +0200

    web proba

--- config/cors.php
+++ config/cors.php
@@ -6,24 +6,22 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
-    'paths' => ['api/*', 'broadcasting/auth'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'http://localhost',
-        'http://localhost:84',
-        'http://localhost:5173'
+        'http://127.0.0.1',
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://localhost:8080',
+        'http://localhost:4200',
     ],
+
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
 
@@ -34,5 +32,4 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
-
 ];
