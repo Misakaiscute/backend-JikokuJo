cd6043a47f01a02be9f191264f24cc62d49938de
3111TAN-05 <3111TAN-05@tanuzem.boronkay.local>
2026-04-01 11:46:21 +0200

    javitas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
     })
     ->withBroadcasting(
         __DIR__.'/../routes/channels.php',
--- config/cors.php
+++ config/cors.php
@@ -14,6 +14,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost',
+        'http://localhost:84',
         'http://127.0.0.1',
         'http://localhost:3000',
         'http://localhost:5173',
